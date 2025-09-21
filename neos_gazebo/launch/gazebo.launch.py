6c2e63188078f2359886955a3c51e30fbe9c22e7
MedChebbi <1.mohamed.chebbi@gmail.com>
2025-09-21 16:54:55 +0200

    Added gazebo models and worlds

---
+++ neos_gazebo/launch/gazebo.launch.py
@@ -0,0 +1,41 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    ros_gz_sim_pkg_path = get_package_share_directory('neos_gazebo')
+    pkg_path = FindPackageShare('neos_gazebo')  # Replace with your own package name
+    gz_launch_path = PathJoinSubstitution([ros_gz_sim_pkg_path, 'launch', 'gz_sim.launch.py'])
+
+    return LaunchDescription([
+        SetEnvironmentVariable(
+            'GZ_SIM_RESOURCE_PATH',
+            PathJoinSubstitution([pkg_path, 'models'])
+        ),
+        SetEnvironmentVariable(
+            'GZ_SIM_PLUGIN_PATH',
+            PathJoinSubstitution([pkg_path, 'plugins'])
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gz_launch_path),
+            launch_arguments={
+                'gz_args': PathJoinSubstitution([pkg_path, 'worlds/empty.sdf']),  # Replace with your own world file
+                'on_exit_shutdown': 'True'
+            }.items(),
+        ),
+
+        # Bridging and remapping Gazebo topics to ROS 2 (replace with your own topics)
+        Node(
+            package='ros_gz_bridge',
+            executable='parameter_bridge',
+            arguments=['/example_imu_topic@sensor_msgs/msg/Imu@gz.msgs.IMU',],
+            remappings=[('/example_imu_topic',
+                         '/remapped_imu_topic'),],
+            output='screen'
+        ),
+    ])
